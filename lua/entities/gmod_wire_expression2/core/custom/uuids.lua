5956fe7ea093226d1871b4ed3eb3879be9115f65
Sunroses <deli73@users.noreply.github.com>
2025-01-19 19:44:21 -0800

    ?

--- lua/entities/gmod_wire_expression2/core/custom/uuids.lua
+++ lua/entities/gmod_wire_expression2/core/custom/uuids.lua
@@ -3,6 +3,7 @@ E2Lib.RegisterExtension("uuid", true, "Get entities by a unique ID.")
 __e2setcost(1)
 e2function number entity:uuid()
   if not IsValid(this) then return self:throw("Invalid entity!", 0) end
+  if not ent.UUID then return self:throw("Missing UUID!", 0) end
   return this.UUID
 end
 
