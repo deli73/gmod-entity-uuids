7de3b824a2ff764d840e58963bd0b24c854c524e
Sunroses <deli73@users.noreply.github.com>
2025-01-21 18:38:01 -0800

    Fixed everything i think

--- lua/autorun/server/entity_uuids.lua
+++ lua/autorun/server/entity_uuids.lua
@@ -1,5 +1,6 @@
 --[[
   UUIDs are stored in an entity's "UUID" key as an integer.
+  (Also, persistent map entities store their source entity's ID as baseMapEnt)
   Allegedly the precision works up to a value of 1e+14 (100 trillion)Should be plenty of values lmao.
   We're just gonna go up to 1e+10 (10 billion)
   Map-created entities get priority access to the numbers less than 10000
@@ -9,21 +10,13 @@
   Note: UUIDs of removed entities are only reserved until server restart. This should be fine i think.
 ]]--
 
-local ents_by_uuid = nil --key = uuid, val = entity reference. associative array basically.
+uuid = {}
+
+local ents_by_uuid = {} --key = uuid, val = entity reference. associative array basically.
+initialized = false
 
 function uuid.GetEntity(id)
   return ents_by_uuid[id]
-end
-
-local function give_new_id(ent)
-  local seed = (SysTime() + ent:GetCreationID()*10)
-  local id = nil
-  repeat --try random numbers until we get a unique one
-    id = math.floor( util.SharedRandom("entity_uuid_gen", 10000, 1e+10, seed) )
-    seed += SysTime() / 10
-  until not ents_by_uuid[id]
-  ent.UUID = id
-  load_with_id(ent) --now that the ID has been given, register it in the list
 end
 
 local function load_with_id(ent)
@@ -32,17 +25,28 @@ local function load_with_id(ent)
     error("[Entity UUIDs] Called load_with_id on entity without an ID set!")
   end
   ents_by_uuid[id] = ent
+  hook.Run("LoadedUUID", ent)
+end
+
+local function give_new_id(ent)
+  local seed = (SysTime() + ent:GetCreationID()*10)
+  local id = nil
+  repeat --try random numbers until we get a unique one
+    id = math.floor( util.SharedRandom("entity_uuid_gen", 10000, 1e+10, seed) )
+    seed = seed + SysTime() / 10
+  until not ents_by_uuid[id]
+  ent.UUID = id
+  load_with_id(ent) --now that the ID has been given, register it in the list
 end
 
 
 hook.Add("OnEntityCreated", "entity uuid on creation", function(ent)
-  if not ents_by_uuid then return end --game not initialized yet, we'll get to it in InitPostEntity
-
   --on creation of new entities and persist-loaded ones, load ID or give new one.
   --...we gotta wait for the entity to finish being initialized before we do this.
   timer.Simple(0, function()
     if not IsValid(ent) or ent:IsPlayer() then return end
-    if ent.UUID then --entity has UUID already, presumably from persistence, register it
+    if ent.UUID and ents_by_uuid[ent.UUID] then return end
+    if ent.UUID then
       load_with_id(ent)
     else --entity needs new UUID
       give_new_id(ent)
@@ -50,20 +54,60 @@ hook.Add("OnEntityCreated", "entity uuid on creation", function(ent)
   end)
 end)
 
-gameevent.Listen("player_connect")
-hook.Add("player_connect", "entity uuid set for player", function(data)
-  local ply = Player(data.userid)
-  if data.bot then --bots get a random ID, since they're created fresh every time
-    give_new_id(ply)
-  else --players use the negative of their account ID as a UUID instead
-    ply.UUID = -ply:AccountID()
-    load_with_id(ply)
+-- override persist load to force UUID data to be read
+-- this is a big hack lmao
+hook.Add("PostGamemodeLoaded", "entity uuid persist load override", function()
+  hook.Add("PersistenceLoad", "PersistenceLoad", function(name)
+    local data = file.Read("persist/" .. game.GetMap() .. "_" .. name .. ".txt")
+    if not data then return end
+
+    local tab = util.JSONToTable(data)
+    if not tab then return end
+    if not tab.Entities then return end
+    if not tab.Constraints then return end
+
+    local entities = duplicator.Paste(nil, tab.Entities, tab.Constraints)
+
+    MsgN("[Entity UUIDs] Loading persistent UUIDs...")
+    local count = 0
+    for k,ent in pairs(entities) do
+      ent:SetPersistent(true)
+      local old_data = tab.Entities[k]
+      if old_data and old_data.UUID then
+        ent.UUID = old_data.UUID
+        count = count + 1
+      end
+    end
+    MsgN("[Entity UUIDs] " .. count .. " persistent IDs loaded." )
+  end)
+end)
+
+hook.Add("PersistenceSave", "entity uuids on save map id fix", function(name)
+  for _, ent in ents.Iterator() do
+    if ent:CreatedByMap() and ent:GetPersistent() then
+      ent.baseMapEnt = ent:MapCreationID()
+      ents_by_uuid[ent.UUID] = nil
+      give_new_id(ent)
+    end
   end
 end)
 
-hook.Add("InitPostEntity", "entity uuid on map load", function()
+gameevent.Listen("player_connect")
+hook.Add("player_connect", "entity uuid set for player", function(data)
+  timer.Simple(0, function()
+    local ply = Player(data.userid)
+    if not IsValid(ply) then return end
+    if data.bot then --bots get a random ID, since they're created fresh every time
+      give_new_id(ply)
+    else --players use the negative of their account ID as a UUID instead
+      ply.UUID = -ply:AccountID()
+      load_with_id(ply)
+    end
+  end)
+end)
+
+local function InitUUIDs()
   MsgN("[Entity UUIDs] Initializing...")
-  ents_by_uuid = {}
   local to_give_id = {}
 
   for _, ent in ents.Iterator() do
@@ -75,17 +119,11 @@ hook.Add("InitPostEntity", "entity uuid on map load", function()
       ent.UUID = ent:MapCreationID()
     end
     
-    if ent.UUID then --load the entity into the list
+    if ent.UUID then --entity has UUID already, register it
       load_with_id(ent)
-    else
-      --if the entity lacks a UUID at this point, we need to give it one...
-      --but only after everything else loads.
-      table.insert(to_give_id, ent)
     end
   end
+  initialized = true
+end
 
-  --once we've loaded everything already saved with a UUID, give new ones to other stuff
-  for _,ent in ipairs(to_give_id) do
-    give_new_id(ent)
-  end
-end)
+hook.Add("InitPostEntity", "entity uuid on map load", InitUUIDs)
--- lua/entities/gmod_wire_expression2/core/custom/uuids.lua
+++ lua/entities/gmod_wire_expression2/core/custom/uuids.lua
@@ -1,17 +1,19 @@
 E2Lib.RegisterExtension("uuid", true, "Get entities by a unique ID.")
 
-__e2setcost(1)
+E2Lib.registerEvent("uuidSet", {{"Entity", "e"}})
+hook.Add("LoadedUUID", "persistent owner bot wire event", function(ent)
+  E2Lib.triggerEvent("uuidSet", ent)
+end)
+
+__e2setcost(5)
 e2function number entity:uuid()
   if not IsValid(this) then return self:throw("Invalid entity!", 0) end
-  if not ent.UUID then return self:throw("Missing UUID!", 0) end
+  if not this.UUID then return self:throw("Missing UUID!", 0) end
   return this.UUID
 end
 
-__e2setcost(3)
+__e2setcost(5)
 e2function entity byUUID(number id)
   local ent uuid.GetEntity(id)
   return IsValid(ent) and ent or nil
 end
-
-E2Helper.Descriptions["uuid(e:)"] = "Gets an entity's persistent unique ID"
-E2Helper.Descriptions["byUUID(n)"] = "Gets the entity associated with the unique ID"
